2b3bfe228bf6a4b6542571041ef601ea6ad8f3be
Zakaria Ridouh <zakridouh@gmail.com>
2019-10-27 22:50:07 -0500

    change namespace declaration to top

--- functions.h
+++ functions.h
@@ -4,28 +4,22 @@
 #include <string>
 #include "functions.h"
 
+using std::string;
+
 struct Pixel {
   int r; // red
   int g; // green
   int b; // blue
 };
 
-// Implemented for you
-
 Pixel** createImage(int width, int height);
 void deleteImage(Pixel** image, int width);
-
-// Implement for part 1
-
 int* createSeam(int length);
 void deleteSeam(int* seam);
-bool loadImage(std::string filename, Pixel** image, int width, int height);
-bool outputImage(std::string filename, Pixel** image, int width, int height);
+bool loadImage(string filename, Pixel** image, int width, int height);
+bool outputImage(string filename, Pixel** image, int width, int height);
 int energy(Pixel** image, int x, int y, int width, int height);
-bool isValidDigit(std::string str);
-
-// Implement for part 2
-
+bool isValidDigit(string str);
 int loadVerticalSeam(Pixel** image, int start_col, int width, int height, int* seam);
 int loadHorizontalSeam(Pixel** image, int start_row, int width, int height, int* seam);
 int* findMinVerticalSeam(Pixel** image, int width, int height);
